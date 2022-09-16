899bed6d92e890d75bf3b586113dd2a6bd15e6ff
ShineEnoki <110322083+ShineEnoki@users.noreply.github.com>
2022-09-16 17:16:42 +0630

    First commit

---
+++ Assignmet1/script.js
@@ -0,0 +1,5 @@
+document.querySelector(".btn").onclick = function () {
+    document
+        .querySelector(".reply")
+        .textContent = "Sorry,due to political situation I cann't provite my CV."
+}
